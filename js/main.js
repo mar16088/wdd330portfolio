9d53ae920d497b826bd265e76615959fcb49f64c
mar16088 <65192533+mar16088@users.noreply.github.com>
2021-09-16 10:55:41 -0700

    main and html

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ window.onload = function() {
     for(var i = tableOfContents.length - 1; i >= 0, i--) {
         let li = document.createElement('li');
         let a = document.createElement('a');
+        a.textContent = tableOfContents[i].label;
         a.textAttribute('href', tableOfContents[i].url);
         li.append(a);
         list.append(li);
