7e4af189c1a58c127849421e89ee2d068206cd27
mar16088 <65192533+mar16088@users.noreply.github.com>
2021-09-20 14:48:19 -0700

    Delete Team Activity directory

--- Team Activity/w07main.js
+++
@@ -1,6 +0,0 @@
-import Hikes from './w07hikes.js';
-//on load grab the array and insert it into the page
-const myHikes = new Hikes('hikes');
-window.addEventListener('load', () => {
-  myHikes.showHikeList();
-});
