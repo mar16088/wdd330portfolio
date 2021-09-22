408a70fc6a01c1d9d3502118ed75b96b8555bf9f
mar16088 <65192533+mar16088@users.noreply.github.com>
2021-09-22 12:26:48 -0700

    fixed main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 window.onload = function() {
-    let list = document.getElementById('tableOfContents') = [
+    let list = document.getElementById('tableOfContents');
+    
+    const tableOfContents = [
         {
-            label: 'Week 01',
-            url: '/wdd330portfolio/W01/'
+            label: 'Week 01'
+            url: '/WDD330Portfolio/week01'
         }
     ];
 
-    for(var i = tableOfContents.length - 1; i >= 0, i--) {
+    for (var i = tableOfContents.length - 1; i >= 0, i--) {
         let li = document.createElement('li');
         let a = document.createElement('a');
         a.textContent = tableOfContents[i].label;
